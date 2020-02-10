2655db8ce712eed1b4d69bc8f6c6fe9363cc1b3c
Markus Scharfenberg <markusscharfenberg@gmx.de>
2020-02-10 13:09:48 +0100

    Version 0.2

---
+++ README.md
@@ -0,0 +1,6 @@
+## Bundesliga-Tippspiel
+Bundesliga-Tippspiel basierend auf der openligadb.API
+
+**Installation**
+-Git-Repository clonen
+-SQL-File in MySQL-Datenbank importieren
---
+++ sql/tippspiel.sql
@@ -0,0 +1,204 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 10. Feb 2020 um 13:07
+-- Server-Version: 10.4.11-MariaDB
+-- PHP-Version: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `tippspiel`
+--
+CREATE DATABASE IF NOT EXISTS `tippspiel` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `tippspiel`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `tipps`
+--
+
+DROP TABLE IF EXISTS `tipps`;
+CREATE TABLE `tipps` (
+  `userid` int(10) UNSIGNED NOT NULL,
+  `spielid` int(10) UNSIGNED NOT NULL,
+  `tore_1` int(10) UNSIGNED NOT NULL,
+  `tore_2` int(10) UNSIGNED NOT NULL,
+  `punkte` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(249) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(2) UNSIGNED NOT NULL DEFAULT 0,
+  `verified` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `resettable` tinyint(1) UNSIGNED NOT NULL DEFAULT 1,
+  `roles_mask` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `registered` int(10) UNSIGNED NOT NULL,
+  `last_login` int(10) UNSIGNED DEFAULT NULL,
+  `force_logout` mediumint(7) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users_confirmations`
+--
+
+DROP TABLE IF EXISTS `users_confirmations`;
+CREATE TABLE `users_confirmations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(249) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `selector` varchar(16) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `token` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `expires` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users_remembered`
+--
+
+DROP TABLE IF EXISTS `users_remembered`;
+CREATE TABLE `users_remembered` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user` int(10) UNSIGNED NOT NULL,
+  `selector` varchar(24) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `token` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `expires` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users_resets`
+--
+
+DROP TABLE IF EXISTS `users_resets`;
+CREATE TABLE `users_resets` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user` int(10) UNSIGNED NOT NULL,
+  `selector` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `token` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `expires` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users_throttling`
+--
+
+DROP TABLE IF EXISTS `users_throttling`;
+CREATE TABLE `users_throttling` (
+  `bucket` varchar(44) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `tokens` float UNSIGNED NOT NULL,
+  `replenished_at` int(10) UNSIGNED NOT NULL,
+  `expires_at` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `tipps`
+--
+ALTER TABLE `tipps`
+  ADD PRIMARY KEY (`userid`,`spielid`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indizes für die Tabelle `users_confirmations`
+--
+ALTER TABLE `users_confirmations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `selector` (`selector`),
+  ADD KEY `email_expires` (`email`,`expires`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indizes für die Tabelle `users_remembered`
+--
+ALTER TABLE `users_remembered`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `selector` (`selector`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indizes für die Tabelle `users_resets`
+--
+ALTER TABLE `users_resets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `selector` (`selector`),
+  ADD KEY `user_expires` (`user`,`expires`);
+
+--
+-- Indizes für die Tabelle `users_throttling`
+--
+ALTER TABLE `users_throttling`
+  ADD PRIMARY KEY (`bucket`),
+  ADD KEY `expires_at` (`expires_at`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `users_confirmations`
+--
+ALTER TABLE `users_confirmations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `users_remembered`
+--
+ALTER TABLE `users_remembered`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `users_resets`
+--
+ALTER TABLE `users_resets`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
